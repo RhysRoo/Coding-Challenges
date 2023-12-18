20a4855be24be5270d49ccc828bf026c7b7fbcd0
RhysRoo <93945223+RhysRoo@users.noreply.github.com>
2023-12-18 20:10:40 +0000

    human_years

---
+++ kyu 8/human_years().py
@@ -0,0 +1,20 @@
+def human_years_cat_years_dog_years(human_years):
+    
+    cat_years = 0
+    dog_years = 0
+    
+    for i in range(human_years + 1): 
+        if i == 1:
+            cat_years += 15
+            dog_years += 15
+        elif i == 2:
+            cat_years += 9
+            dog_years += 9
+        elif i > 2:
+                cat_years += 4
+                dog_years += 5
+    
+    return [human_years, cat_years, dog_years]
+
+if __name__ == '__main__':
+     print(human_years_cat_years_dog_years(10))
