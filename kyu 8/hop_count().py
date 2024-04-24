5e5907de4a3928bfcd64bc2314ec08c3f71e110e
RhysRoo <93945223+RhysRoo@users.noreply.github.com>
2024-04-24 14:17:57 +0100

    hop_count().py

---
+++ kyu 8/hop_count().py
@@ -0,0 +1,15 @@
+def hoop_count(n):
+    
+    if n < 10:
+        while n < 10:
+            print("Keep at it until you get it")
+            n += 1
+
+    if n > 10:
+            print("Great, now move on to tricks")
+
+    else:
+            print("Great, now move on to tricks") 
+    
+if __name__ == "__main__":
+        print(hoop_count(3))
